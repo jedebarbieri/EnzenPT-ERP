2b6862342de9c30135f3068fc13f6ce78ff6a190
Justo Debarbieri <jedebarbieri@gmail.com>
2023-10-13 16:57:47 +0100

    Main Sidebar Menu - First version

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -5,3 +5,27 @@
         <p>Home</p>
     </a>
 </li>
+<li class="nav-item">
+    <a href="{{ route('procurement') }}" class="nav-link {{ Request::is('procurement') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-cart-plus"></i>
+        <p>Procurement</i></p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="{{ route('procurement') }}" class="nav-link {{ Request::is('clients') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-users"></i>
+        <p>Clients</i></p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="{{ route('procurement') }}" class="nav-link {{ Request::is('budgets') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-file-contract"></i>
+        <p>Budgets</i></p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="{{ route('procurement') }}" class="nav-link {{ Request::is('projects') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-shopping-cart"></i>
+        <p>Projects</i></p>
+    </a>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/procurement', [App\Http\Controllers\Procurement\ProcurementController::class, 'index'])->name('procurement');
 
 Auth::routes();
