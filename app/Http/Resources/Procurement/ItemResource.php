1a65f75c0ec2040d07cb30ec68327486afb940ae
Justo Debarbieri <jedebarbieri@gmail.com>
2023-10-17 14:57:49 +0100

    Item - Resources and Requests

---
+++ app/Http/Resources/Procurement/ItemResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Procurement;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ItemResource extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'internalCod' => $this->internalCod,
+            'unitPrice' => $this->unitPrice
+        ];
+    }
+}
