fe22057b0a9fecf1b59616f7e88c0b143be6e184
Justo Debarbieri <jedebarbieri@gmail.com>
2023-10-30 12:18:33 +0000

    Fixing styles for fullscreen modals

--- resources/views/app/components/BudgetModal.blade.php
+++ resources/views/app/components/BudgetModal.blade.php
@@ -8,7 +8,7 @@
 
 <div class="modal fade" id="{{ $modalId }}" tabindex="-1" aria-labelledby="{{ $modalId }}Label"
     aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable modal-xl">
+    <div class="modal-dialog modal-dialog-scrollable modal-xxl">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="{{ $modalId }}Label">New Budget</h5>
