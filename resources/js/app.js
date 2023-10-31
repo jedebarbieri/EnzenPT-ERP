743a1505b1686de9aba340f53ccdcfd6396be526
Justo Debarbieri <jedebarbieri@gmail.com>
2023-10-31 12:38:20 +0000

    Installing Inputmask

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,6 +17,9 @@ import 'datatables.net-buttons-bs4'
 // Select2 Library
 import select2 from 'select2'; select2();
 
+import Inputmask from "inputmask";
+Inputmask();
+
 import 'admin-lte';
 
 import './common';
