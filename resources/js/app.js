0aae4455196202f263f1f55540a3f0967abbf6d0
Justo Debarbieri <jedebarbieri@gmail.com>
2023-11-14 13:08:35 +0000

    BUG: Adding Authorization on the axios header

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,15 @@
 import jQuery from 'jquery'
 window.$ = jQuery;
 
+import './bootstrap';
+
 $.ajaxSetup({
     beforeSend: function(xhr) {
         xhr.setRequestHeader('Authorization', 'Bearer ' + window.Laravel.apiToken);
     }
 });
 
-import './bootstrap';
+window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + window.Laravel.apiToken;
 
 // Validation Library
 import 'jquery-validation';
