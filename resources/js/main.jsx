4aa874f85087679dfca7c851fb869db189f7c3de
Justo Debarbieri <jedebarbieri@gmail.com>
2023-11-16 11:00:35 +0000

    Compiling with buils command. React is working

---
+++ resources/js/main.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log("Hola Mundo");
+
+function App() {
+    return <h1>Hola Mundo</h1>;
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
+
+
+
+
+
+// import jQuery from 'jquery'
+// window.$ = jQuery;
+
+// import './bootstrap';
+
+// $.ajaxSetup({
+//     beforeSend: function(xhr) {
+//         xhr.setRequestHeader('Authorization', 'Bearer ' + window.Laravel.apiToken);
+//     }
+// });
+
+// window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + window.Laravel.apiToken;
+
+// // Validation Library
+// import 'jquery-validation';
+
+// // DataTable Library
+// import 'datatables.net'
+// import 'datatables.net-responsive'
+// import 'datatables.net-buttons'
+// import 'datatables.net-bs4'
+// import 'datatables.net-responsive-bs4'
+// import 'datatables.net-buttons-bs4'
+
+// // Select2 Library
+// import select2 from 'select2'; select2();
+
+// import Inputmask from "inputmask";
+// Inputmask();
+
+// import 'admin-lte';
+
+// import MaskedInput from "./MaskedInput";
+// window.MaskedInput = MaskedInput;
+// new MaskedInput({});
+
+// import './common';
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+        react(),
     ],
+    build: {
+        outDir: 'resources/build', // establece el directorio de salida para los archivos compilados
+        rollupOptions: {
+            input: 'resources/js/main.jsx' // archivo de entrada para el proceso de compilación
+        },
+        manifest: true, // genera un manifest.json con los nombres de los archivos compilados
+    },
     resolve: {
         alias: [
             {
